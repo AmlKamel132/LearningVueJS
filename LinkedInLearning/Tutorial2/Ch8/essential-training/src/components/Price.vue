e8f1f15777de528386390e4f6101a50fa4b35097
AmlKamel132 <56166672+AmlKamel132@users.noreply.github.com>
2021-12-25 11:51:55 +0200

    Tutorial2_Ch8_End_Tutorial

---
+++ LinkedInLearning/Tutorial2/Ch8/essential-training/src/components/Price.vue
@@ -0,0 +1,28 @@
+   <template>
+  <span>
+    {{ prefix + Number.parseFloat(value * conversion).toFixed(precision) }}
+  </span>
+</template>
+<script>
+export default {
+  props: {
+    value: Number,
+    prefix: {
+      type: String, // type of prop
+      default: "$", // //default value case not passed
+    },
+    precision: {
+      type: Number,
+      default: 2,
+    },
+    conversion: {
+      type: Number,
+      default: 1,
+    },
+  },
+};
+</script>
+
+
+   
+ 
---
+++ LinkedInLearning/Tutorial2/Ch8/essential-training/src/components/PriceSlider.vue
@@ -0,0 +1,46 @@
+<template>
+  <transition
+    name="custome"
+    enter-active-class="animated fadeInDown"
+    leave-active-class="animated slideOutRight"
+  >
+    <div v-if="sliderStatus">
+      <div class="align-items-center" :class="sliderState">
+        <label :class="labelArr" for="formMax">max</label>
+        <input
+          type="text"
+          id="formMax"
+          class="form-control mx-2 text-center"
+          :style="{ width: inputWidth + 'px', 'text-align': 'center' }"
+          v-model="maxAmount"
+          @change="$parent.$emit('update:maximum', maxAmount)"
+        />
+        <input
+          type="range"
+          class="custom-range"
+          min="0"
+          max="200"
+          v-model="maxAmount"
+          @input="$parent.$emit('update:maximum', maxAmount)"
+        />
+      </div>
+    </div>
+  </transition>
+</template>
+<script>
+export default {
+  props: ["sliderStatus"],
+  data() {
+    return {
+      labelArr: ["font-weight-bold", "mr-2"],
+      inputWidth: 60,
+      maxAmount: 99,
+    };
+  },
+  computed: {
+    sliderState: function () {
+      return this.sliderStatus ? "d-flex" : "d-none";
+    },
+  },
+};
+</script>
